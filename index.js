a9356711dd8d776b34b30c342e8d3f2df0af7650
illia Voitenko <illia@mbp-illia.localdomain>
2023-10-11 15:03:58 +0200

    added test logout socket

--- index.js
+++ index.js
@@ -17,10 +17,16 @@ socketIO.on("connection", (socket) => {
   socket.on("message", (data) => {
     socketIO.emit("response", data);
   });
+  socket.on("logoutUser", (data) => {
+    console.log("logoutUser :", data);
+    users.filter((user) => user.socketID !== data.socketID);
+  });
   socket.on("newUser", (newUser) => {
     users.push(newUser);
     socketIO.emit("responseNewUser", users);
+    console.log("after logout :", users);
   });
+  socket.on("typing", (data) => socket.broadcast.emit("respnseTyping", data));
   socket.on("disconnect", () => {
     console.log(`${socket.id} user is disconnected `);
   });
