f0e1b6b8a8898f2e016730e62378b9431f9a7dee
illia Voitenko <illia@mbp-illia.localdomain>
2023-10-11 16:31:24 +0200

    check delete offline users

--- index.js
+++ index.js
@@ -11,20 +11,24 @@ const socketIO = require("socket.io")(http, {
     origin: "http://localhost:5173",
   },
 });
-const users = [];
+let users = [];
+let currentUser = null;
+
 socketIO.on("connection", (socket) => {
   console.log(`${socket.id} user connected  `);
   socket.on("message", (data) => {
     socketIO.emit("response", data);
   });
-  socket.on("logoutUser", (data) => {
-    console.log("logoutUser :", data);
-    users.filter((user) => user.socketID !== data.socketID);
+  socket.on("currentUser", (user) => (currentUser = user));
+  socket.on("logoutUser", () => {
+    const filtered = users.filter(
+      (user) => user.socketID !== currentUser.socketID
+    );
+    users = filtered;
   });
   socket.on("newUser", (newUser) => {
     users.push(newUser);
     socketIO.emit("responseNewUser", users);
-    console.log("after logout :", users);
   });
   socket.on("typing", (data) => socket.broadcast.emit("respnseTyping", data));
   socket.on("disconnect", () => {
